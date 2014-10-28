7e8164d2d06fdec71e265b39026ef000a6fdeded
Michael Martin Moro <michael@unetresgrossebite.com>
2014-10-28 10:43:57 +0100

    adds formula for Crails

---
+++ crails.rb
@@ -0,0 +1,28 @@
+require "formula"
+
+class Crails < Formula
+  homepage "https://github.com/Plaristote/crails"
+
+  head 'https://github.com/Plaristote/crails.git'
+
+  depends_on 'cmake' => :build
+
+  depends_on 'boost'
+  depends_on 'cpp-netlib'
+
+  def install
+    args = std_cmake_args
+
+    # Install Boots
+    system 'cd Boots'
+    system 'cmake', '.'
+    system 'make', 'install'
+    prefix.install 'install_manifest.txt'
+    system 'cd ..'
+
+    # Install Crails
+    system 'cmake', '.', *args
+    system 'make', 'install'
+    prefix.install 'install_manifest.txt'
+  end
+end
