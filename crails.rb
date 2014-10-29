8ec83063ff1437d7bda664869398dd7c6675a5eb
Michael Martin Moro <michael@unetresgrossebite.com>
2014-10-29 13:00:20 +0100

    * fixed compiling of Boots * adds options to build the async server * adds options to build with mongo-cxx-driver or soci backends

--- crails.rb
+++ crails.rb
@@ -13,15 +13,20 @@ class Crails < Formula
   depends_on 'mongo-cxx-driver' => :optional
   depends_on 'soci'             => :optional
 
+  option 'async', 'compile with multi-thread support'
+
   def install
-    args = std_cmake_args
+    args  = std_cmake_args
+    args << '-DUSE_MONGODB=ON' if build.with?    'mongo-cxx-driver'
+    args << '-DUSE_SOCI=ON'    if build.with?    'soci'
+    args << '-DUSE_ASYNC=ON'   if build.include? 'async'
 
     # Install Boots
-    system 'cd Boots'
+    system 'cd', 'Boots'
     system 'cmake', '.'
     system 'make', 'install'
     prefix.install 'install_manifest.txt'
-    system 'cd ..'
+    system 'cd', '..'
 
     # Install Crails
     system 'cmake', '.', *args
